89cb592d362c127c34f6ffd13e97305aabd2b070
Victor Szalma <vszalma@hotmail.com>
2024-12-29 19:21:59 -0500

    added docs and argparse for cleaner argument handling.

--- compare.py
+++ compare.py
@@ -1,3 +1,4 @@
+import argparse
 import hashlib
 import sys
 import home_automation_common
@@ -5,7 +6,56 @@ from datetime import datetime
 import structlog
 
 
+def _get_arguments():
+    """
+    Parses command-line arguments for file comparison.
+    Returns:
+        argparse.Namespace: A namespace object containing the parsed arguments.
+    Arguments:
+        --file1, -f1 (str, required): Path to the file to compare to file2.
+        --file2, -f2 (str, required): Path to the file to be compared to file1.
+    """
+    parser = argparse.ArgumentParser(
+        description="Process a directory and file type for file operations."
+    )
+
+    # Add named arguments
+    parser.add_argument(
+        "--file1",
+        "-f1",
+        type=str,
+        required=True,
+        help="Path to the file to compare to file2.",
+    )
+    parser.add_argument(
+        "--file2",
+        "-f2",
+        type=str,
+        required=True,
+        help="Path to the file to be compared to file1.",
+    )
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Return arguments as a namespace object
+    return args
+
+
 def _calculate_file_hash(file_path, hash_algorithm="sha256"):
+    """
+    Calculate the hash of a file using the specified hash algorithm.
+
+    Args:
+        file_path (str): The path to the file for which the hash is to be calculated.
+        hash_algorithm (str, optional): The hash algorithm to use (default is "sha256").
+
+    Returns:
+        str: The hexadecimal digest of the file's hash.
+
+    Raises:
+        Exception: If the file is not found or an error occurs during hash calculation.
+    """
     try:
         # Create a hash object
         hash_func = hashlib.new(hash_algorithm)
@@ -21,6 +71,17 @@ def _calculate_file_hash(file_path, hash_algorithm="sha256"):
 
 
 def compare_files(file1, file2, hash_algorithm="sha256"):
+    """
+    Compare the hashes of two files to determine if they are identical.
+    Args:
+        file1 (str): The path to the first file.
+        file2 (str): The path to the second file.
+        hash_algorithm (str, optional): The hash algorithm to use for comparison. Defaults to "sha256".
+    Returns:
+        bool: True if the files have the same hash, False otherwise.
+    Raises:
+        Exception: If an error occurs during file comparison, it logs the error and returns False.
+    """
 
     logger = structlog.get_logger()
 
@@ -29,25 +90,13 @@ def compare_files(file1, file2, hash_algorithm="sha256"):
         hash2 = _calculate_file_hash(file2, hash_algorithm)
         return hash1 == hash2
     except Exception as e:
-        logger.error("File comparison error.", module="compare._calculate_file_hash", message="An error occurred while comparing files", exception=e)
-        return False
-
-
-def get_arguments(argv):
-    arg_help = "{0} <file1> <file2>".format(argv[0])
-
-    try:
-        file1 = (
-            sys.argv[1]
-            if len(sys.argv) > 1
-            else '"C:\\Users\\vszal\\OneDrive\\Pictures"'
+        logger.error(
+            "File comparison error.",
+            module="compare._calculate_file_hash",
+            message="An error occurred while comparing files",
+            exception=e,
         )
-        file2 = sys.argv[2] if len(sys.argv) > 2 else "image"
-    except:
-        print(arg_help)
-        sys.exit(2)
-
-    return [file1, file2]
+        return False
 
 
 if __name__ == "__main__":
@@ -62,17 +111,24 @@ if __name__ == "__main__":
 
     logger = structlog.get_logger()
 
-    arguments = get_arguments(sys.argv)
-    logger.info("File comparison starting.", module="compare.__main__", message="Files to be compared.", file1=arguments[0], file2=arguments[1])
+    args = _get_arguments()
+    logger.info(
+        "File comparison starting.",
+        module="compare.__main__",
+        message="Files to be compared.",
+        file1=args.file1,
+        file2=args.file2,
+    )
 
-    if compare_files(arguments[0], arguments[1]):
-        logger.info("File comparison successful", module="compare.__main__", message="The files are identical.")
+    if compare_files(args.file1, args.file2):
+        logger.info(
+            "File comparison successful",
+            module="compare.__main__",
+            message="The files are identical.",
+        )
     else:
-        logger.warning("File comparison failed.", module="compare.__main__", message="The files are different.")
-
-    # home_automation_common.send_email(
-    #     subject="Test Email from Python",
-    #     body="This is a test email.",
-    #     to_email="vszalma@hotmail.com",
-    #     from_email="vszalma@hotmail.com",
-    # )
+        logger.warning(
+            "File comparison failed.",
+            module="compare.__main__",
+            message="The files are different.",
+        )
--- validate_file.py
+++ validate_file.py
@@ -9,6 +9,7 @@ import logging
 from datetime import datetime, timedelta
 from time import time
 import home_automation_common
+import argparse
 
 warnings.simplefilter("ignore", UserWarning)
 
@@ -24,24 +25,53 @@ FILE_TYPE_GROUPS = {
 }
 
 
-def _get_arguments(argv):
-    arg_help = "{0} <directory> <filetype>".format(argv[0])
+def _get_arguments():
+    """
+    Parses command-line arguments for directory and file type.
+    Returns:
+        argparse.Namespace: A namespace object containing the parsed arguments.
+            - directory (str): Path to the directory to process. Defaults to 'F:\\'.
+            - filetype (str): Type of files to process (e.g., 'image', 'video', etc.). Defaults to 'image'.
+    """
+    parser = argparse.ArgumentParser(
+        description="Process a directory and file type for file operations."
+    )
 
-    try:
-        arg_directory = (
-            sys.argv[1]
-            if len(sys.argv) > 1
-            else '"C:\\Users\\vszal\\OneDrive\\Pictures"'
-        )
-        arg_filetypes = sys.argv[2] if len(sys.argv) > 2 else "image"
-    except:
-        print(arg_help)
-        sys.exit(2)
+    # Add named arguments
+    parser.add_argument(
+        "--directory",
+        "-d",
+        type=str,
+        default="F:\\",
+        help="Path to the directory to process. Defaults to 'F:\\'.",
+    )
+    parser.add_argument(
+        "--filetype",
+        "-f",
+        type=str,
+        default="image",
+        help="Type of files to process (e.g., 'image', 'video', etc.). Defaults to 'image'.",
+    )
 
-    return [arg_directory, arg_filetypes]
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Return arguments as a namespace object
+    return args
 
 
 def _validate_document(file_path):
+    """
+    Validates a Word document file.
+    This function attempts to open a Word document file using the `python-docx` library.
+    It checks if the document contains any paragraphs to determine its validity.
+    Args:
+        file_path (str): The path to the Word document file to be validated.
+    Returns:
+        tuple: A tuple containing a boolean and a string message.
+               - The boolean indicates whether the document is valid (True) or not (False).
+               - The string message provides additional information about the validation result.
+    """
     from docx import Document
 
     try:
@@ -59,6 +89,18 @@ def _validate_document(file_path):
 
 
 def _validate_audio(file_path):
+    """
+    Validates if the given file path points to a valid audio file.
+    This function attempts to load the audio file using the pydub library.
+    If the file is successfully loaded, it is considered a valid audio file.
+    Otherwise, it is considered invalid.
+    Args:
+        file_path (str): The path to the audio file to be validated.
+    Returns:
+        tuple: A tuple containing a boolean and a string.
+               The boolean indicates whether the file is a valid audio file.
+               The string provides a message with the validation result.
+    """
     from pydub import AudioSegment
 
     try:
@@ -70,6 +112,15 @@ def _validate_audio(file_path):
 
 
 def _validate_excel(file_path):
+    """
+    Validates an Excel (.xlsx) file by attempting to open it and checking for the presence of sheets.
+    Args:
+        file_path (str): The path to the Excel file to be validated.
+    Returns:
+        tuple: A tuple containing a boolean and a string.
+            - bool: True if the file is a valid .xlsx file with at least one sheet, False otherwise.
+            - str: A message indicating the result of the validation.
+    """
     from openpyxl import load_workbook
 
     try:
@@ -85,6 +136,22 @@ def _validate_excel(file_path):
 
 
 def _validate_pdf(file_path):
+    """
+    Validates a PDF file.
+    This function checks if the provided file path points to a valid PDF file.
+    It uses the PyPDF2 library to read the PDF and determine if it is valid,
+    has pages, and whether it is encrypted.
+    Args:
+        file_path (str): The path to the PDF file to be validated.
+    Returns:
+        tuple: A tuple containing a boolean and a string message.
+               The boolean indicates whether the PDF is valid.
+               The string provides additional information about the validation result.
+               - (True, "File is encrypted") if the PDF is valid and encrypted.
+               - (True, "Valid pdf file.") if the PDF is valid and not encrypted.
+               - (False, "Invalid PDF: {file_path} (No pages found)") if the PDF has no pages.
+               - (False, "Invalid PDF: {file_path} (Error: {e})") if an error occurred during validation.
+    """
     from contextlib import redirect_stderr
     import io
 
@@ -108,6 +175,18 @@ def _validate_pdf(file_path):
 
 
 def _validate_video(file_path):
+    """
+    Validates a video file using FFprobe.
+    This function runs the FFprobe command to check the duration of the video file,
+    which serves as an indicator of its validity. If the command executes successfully,
+    the video file is considered valid.
+    Args:
+        file_path (str): The path to the video file to be validated.
+    Returns:
+        tuple: A tuple containing a boolean and a string message.
+               - True and "Valid video file." if the video file is valid.
+               - False and an error message if the video file is not valid.
+    """
     import subprocess
 
     try:
@@ -129,6 +208,17 @@ def _validate_video(file_path):
 
 
 def _validate_image(file_path):
+    """
+    Validates whether the given file path points to a valid image file.
+
+    Args:
+        file_path (str): The path to the image file to be validated.
+
+    Returns:
+        tuple: A tuple containing a boolean and a string message.
+               The boolean is True if the file is a valid image, False otherwise.
+               The string message provides additional information about the validation result.
+    """
     try:
         with Image.open(file_path) as img:
             img.verify()  # Verify that it is a valid image
@@ -138,6 +228,14 @@ def _validate_image(file_path):
 
 
 def _get_total_file_count(directory, exclusions):
+    """
+    Calculate the total number of files in a directory, excluding specified files and directories.
+    Args:
+        directory (str): The root directory to start the file count.
+        exclusions (list): A list of file and directory paths to exclude from the count.
+    Returns:
+        int: The total number of files in the directory, excluding the specified exclusions.
+    """
     file_count = 0
 
     for root, dirs, files in os.walk(directory):
@@ -154,12 +252,34 @@ def _get_total_file_count(directory, exclusions):
 
 
 def _normalize_path(path, directory=None):
+    """
+    Normalize the given file path.
+
+    If a directory is provided, it will be prepended to the path before normalization.
+
+    Args:
+        path (str): The file path to normalize.
+        directory (str, optional): The directory to prepend to the path. Defaults to None.
+
+    Returns:
+        str: The normalized file path.
+    """
     if directory:
         path = f"{directory}{path}"
     return os.path.normpath(path)
 
 
 def _get_file_count_for_type(directory, compiled_pattern, exclusions):
+    """
+    Count the number of files in a directory and its subdirectories that match a given pattern,
+    excluding specified files and directories.
+    Args:
+        directory (str): The root directory to start the search.
+        compiled_pattern (re.Pattern): The compiled regular expression pattern to match file names.
+        exclusions (list): A list of file and directory paths to exclude from the count.
+    Returns:
+        int: The count of files that match the pattern and are not in the exclusions list.
+    """
     file_count = 0
 
     # Walk through the directory and subdirectories
@@ -179,6 +299,19 @@ def _get_file_count_for_type(directory, compiled_pattern, exclusions):
 def _write_summary_file(
     file_type_or_group, total_files, file_count, error_count, start_time, end_time
 ):
+    """
+    Writes a summary of the file validation process to a CSV file.
+    Args:
+        file_type_or_group (str): The type or group of files being validated.
+        total_files (int): The total number of files processed.
+        file_count (int): The number of files that matched the criteria.
+        error_count (int): The number of files that encountered errors.
+        start_time (datetime): The start time of the validation process.
+        end_time (datetime): The end time of the validation process.
+    Returns:
+        None
+    """
+
     summary_headers = [
         "date",
         "file_type",
@@ -209,6 +342,15 @@ def _write_summary_file(
 
 
 def _append_to_csv(file_path, data, headers=None):
+    """
+    Appends data to a CSV file. If the file does not exist, it creates a new one and optionally writes headers.
+    Args:
+        file_path (str): The path to the CSV file.
+        data (list): The data to append to the CSV file.
+        headers (list, optional): The headers to write if the file is newly created. Defaults to None.
+    Returns:
+        None
+    """
 
     output_file = home_automation_common.get_full_filename("output", file_path)
 
@@ -226,13 +368,48 @@ def _append_to_csv(file_path, data, headers=None):
 
 
 def validate_files_by_type(start_folder, file_type_or_group):
+    """
+    Validates files in a given directory based on their type or group.
+    Args:
+        start_folder (str): The directory to search for files.
+        file_type_or_group (str or list): The file type or group to validate.
+                                          Can be a string representing a single file type or a group,
+                                          or a list of file types.
+    Raises:
+        ValueError: If the file_type_or_group is not a string or list, or if the start_folder does not exist.
+    Logs:
+        Various informational and error messages during the validation process.
+    The function performs the following steps:
+        1. Logs the start of the search.
+        2. Resolves regex patterns based on the file type or group.
+        3. Checks if the start folder exists.
+        4. Compiles all patterns into a single regex for efficiency.
+        5. Loads exclusions from an exclusion file.
+        6. Logs the beginning of the file type search.
+        7. Counts the total files and matching files.
+        8. Logs the number of matching files found.
+        9. Writes headers to an output CSV file.
+        10. Walks through the directory and subdirectories to validate files.
+        11. Logs any validation exceptions found.
+        12. Removes the output file if no errors are found.
+        13. Writes a summary file.
+        14. Logs the completion of the validation.
+    Returns:
+        None
+    """
     from tqdm import tqdm
 
     start_time = datetime.now().time()
 
     logger = structlog.get_logger()
 
-    logger.info("Searching for file types.", module="validate_file.validate_files_by_type", message="Directory to be searched for file types.", folder=start_folder, type=file_type_or_group)
+    logger.info(
+        "Searching for file types.",
+        module="validate_file.validate_files_by_type",
+        message="Directory to be searched for file types.",
+        folder=start_folder,
+        type=file_type_or_group,
+    )
 
     # Resolve regex patterns based on group or specific type
     if isinstance(file_type_or_group, str):
@@ -242,11 +419,19 @@ def validate_files_by_type(start_folder, file_type_or_group):
     elif isinstance(file_type_or_group, list):
         patterns = file_type_or_group
     else:
-        logger.error("Invalid file type argument.", module="validate_file.validate_files_by_type", message="file_type_or_group must be a string or a list")
+        logger.error(
+            "Invalid file type argument.",
+            module="validate_file.validate_files_by_type",
+            message="file_type_or_group must be a string or a list",
+        )
         raise ValueError("file_type_or_group must be a string or a list")
 
     if not os.path.exists(start_folder):
-        logger.error("Folder does not exist", module="validate_file.validate_files_by_type", message=f"Folder {start_folder} does not exist. Retry.")
+        logger.error(
+            "Folder does not exist",
+            module="validate_file.validate_files_by_type",
+            message=f"Folder {start_folder} does not exist. Retry.",
+        )
         raise ValueError("Invalid folder was specified. It does not exist.")
 
     # Compile all patterns into a single regex for efficiency
@@ -267,24 +452,32 @@ def validate_files_by_type(start_folder, file_type_or_group):
                 )
         except FileNotFoundError:
             logger.info(
-                "No exclusions found.", module="validate_file.validate_files_by_type", message=f"Exclusion file {exclusion_file} not found. Continuing without exclusions."
+                "No exclusions found.",
+                module="validate_file.validate_files_by_type",
+                message=f"Exclusion file {exclusion_file} not found. Continuing without exclusions.",
             )
 
     file_count = 0
     error_count = 0
-    logger.info("File type search beginning.", module="validate_file.validate_files_by_type", message=f"Looking for {file_type_or_group} files in directory {start_folder}.")
+    logger.info(
+        "File type search beginning.",
+        module="validate_file.validate_files_by_type",
+        message=f"Looking for {file_type_or_group} files in directory {start_folder}.",
+    )
 
     total_files = _get_total_file_count(start_folder, exclusions)
     matching_files = _get_file_count_for_type(
         start_folder, combined_pattern, exclusions
     )
 
-    logger.info("Matching files found.", module="validate_file.validate_files_by_type", message=f"Found a total of {matching_files} found. Validating files now.")
+    logger.info(
+        "Matching files found.",
+        module="validate_file.validate_files_by_type",
+        message=f"Found a total of {matching_files} found. Validating files now.",
+    )
 
     headers = ["file_name", "error_message"]
-    output_file = (
-        f"{datetime.now().date()}-error-output-{file_type_or_group}.csv"
-    )
+    output_file = f"{datetime.now().date()}-error-output-{file_type_or_group}.csv"
 
     output_file = home_automation_common.get_full_filename("output", output_file)
 
@@ -325,14 +518,23 @@ def validate_files_by_type(start_folder, file_type_or_group):
                             case "document":
                                 is_valid, error_message = _validate_document(file_path)
                             case _:
-                                logger.error("Incorrect file type found.", module="validate_file.validate_files_by_type", message="An undefined filetype is found")
+                                logger.error(
+                                    "Incorrect file type found.",
+                                    module="validate_file.validate_files_by_type",
+                                    message="An undefined filetype is found",
+                                )
                                 is_valid, error_message = False, "Undefined filetype"
 
                         pbar.update(1)
                         if not is_valid:
                             error_count += 1
                             writer.writerow([file_path, error_message])
-                            logger.info("File validation exception found.", module="validate_file.validate_files_by_type", message=error_message, file=file_path)
+                            logger.info(
+                                "File validation exception found.",
+                                module="validate_file.validate_files_by_type",
+                                message=error_message,
+                                file=file_path,
+                            )
 
     if error_count == 0:
         os.remove(output_file)
@@ -344,7 +546,9 @@ def validate_files_by_type(start_folder, file_type_or_group):
     )
 
     logger.info(
-        "Validation completed.", module="validate_file.validate_files_by_type", message="Analysis complete.",
+        "Validation completed.",
+        module="validate_file.validate_files_by_type",
+        message="Analysis complete.",
         total_files=total_files,
         matching_files=matching_files,
         error_count=error_count,
@@ -363,10 +567,7 @@ if __name__ == "__main__":
 
     logger = structlog.get_logger()
 
-    arguments = _get_arguments(sys.argv)
+    args = _get_arguments()
 
-    if len(arguments) != 2:
-        logger.error("Invalid arguments.", module="validate_file.__main__", message="Invalid arguments.")
-    else:
-        # Validate files
-        validate_files_by_type(arguments[0], arguments[1])
+    # Validate files
+    validate_files_by_type(args.directory, args.filetype)
