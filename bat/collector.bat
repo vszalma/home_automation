cee2b51da7175d88b71b4b3c7c7b4814d6fb10bb
Victor Szalma <vszalma@hotmail.com>
2025-06-28 12:59:27 -0400

    changes to bat files

--- bat/collector.bat
+++ bat/collector.bat
@@ -4,5 +4,5 @@
     @REM     --directory (str): Path to the directory to process.
 
 C:\Users\vszal\Documents\code\home_automation\.venv\Scripts\python.exe ^
-    C:\Users\vszal\Documents\code\home_automation\collector.py --directory D:\_bu-2024-11-16\_apps  ^
+    C:\Users\vszal\Documents\code\home_automation\collector.py --directory N:  ^
     > C:\Users\vszal\Documents\code\home_automation\log\collector_bat.log 2>&1
