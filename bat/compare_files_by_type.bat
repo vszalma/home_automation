97c814b33fef530601becfe669f0c55ded18a893
Victor Szalma <vszalma@hotmail.com>
2025-11-13 21:34:16 -0500

    fixed incorrect path in .bat file.

--- bat/compare_files_by_type.bat
+++ bat/compare_files_by_type.bat
@@ -6,8 +6,6 @@
     @REM     filetype (str): The file type to search for differences in the two file structures. Defaults to ".jpg".
 
 set LOGROOT=C:\home_automation\log
-set LOGFILE=%LOGROOT%\compare_files_by_type_bat.log
-set DEBUGLOG=%LOGROOT%\debug.log
 set PYTHON_EXE=C:\python313\python.exe
 
-%PYTHON_EXE% C:\home_automation\compare_files_by_type.py --directory1 E:\Backups --directory2  N:\ --filetype .db >> %LOGFILE% 2>&1
+%PYTHON_EXE% C:\home_automation\compare_files_by_type.py --directory1 E:\Backups\BU-2025-11-13 --directory2  N:\ --filetype .db
