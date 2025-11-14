04b4c1b7c2b6b548a6d7810806644b20f09909d8
Victor Szalma <vszalma@hotmail.com>
2025-11-13 22:32:10 -0500

    Minor changes to copy_master.bat

--- bat/copy_master.bat
+++ bat/copy_master.bat
@@ -2,16 +2,16 @@
 REM filepath: C:\Users\vszal\Documents\code\home_automation\bat\copy_master.bat
 
 REM Set Python executable from virtual environment
-set PYTHON_EXE=C:\Users\vszal\Documents\code\home_automation\.venv\Scripts\python.exe
+set PYTHON_EXE=E:\vszalma\Source\Repos\home-automation\home_automation\.venv\Scripts\python.exe
 
 REM Change directory to project root so relative imports work
-cd /d C:\Users\vszal\Documents\code\home_automation
+cd /d E:\vszalma\Source\Repos\home-automation\home_automation\
 
 REM Display what is being executed
 echo Running copy_master.py script...
 echo Python: %PYTHON_EXE%
 
 REM Call script interactively (console remains open)
-%PYTHON_EXE% copy_master.py --source J:\ --destination N:\CathyJ
+%PYTHON_EXE% copy_master.py --source Y:\ --destination Z:\CathyK
 
 pause
